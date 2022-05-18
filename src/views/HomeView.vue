b08e483d309e03869b34f83cb5f2f7a7df1a914b
gcnova <zhangnova927@foxmail.com>
2022-05-19 02:55:47 +0800

    初始化项目

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,15 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <span>我是首页balabala</span>
+    <router-link to="/about">
+      <div>去到关于</div>
+      </router-link>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+  name: 'HomeView'
 }
 </script>
